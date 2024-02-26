35d222696c916878bc54c3d8e1b2ab67396f5572
YashGitt10 <yashrohitgupt@gmail.com>
2024-02-26 15:06:10 +0530

    Y1

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { apiUrl, filterData } from "./data";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
+
+const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title); 
+
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      //output
+      setCourses(output.data);
+    }
+    catch(error) {
+      toast.error("Network Error");
+    }
+    setLoading(false);
+  }
+
+
+  useEffect(() => {
+    fetchData();
+  },[])
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <diV>
+        <Navbar></Navbar>
+      </diV>
+      <div className="bg-bgDark2">
+        <diV>
+          <Filter filterData = {filterData} category= {category} setCategory = {setCategory}></Filter>
+        </diV>
+        <diV className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+          {
+            loading ? (<Spinner></Spinner>) : (<Cards courses = {courses} category={category}></Cards>)
+          }
+        </diV>
+      </div>
+      
+    </div>
+  );
+};
+
+export default App;
